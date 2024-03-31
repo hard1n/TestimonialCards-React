7a0de068ef5005b83c4a6bfc87cdb6e3ad18126e
Hardin <hardisonpaulino@gmail.com>
2024-03-31 08:58:28 -0400

    Feat: Testimonials component; dinamically highlight

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ function App() {
             name: "Amazon",
             job: "Ingeniero de software",
           }}
-          comment="Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."
+          comment={`Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.`}
+          highlight="freeCodeCamp cambió mi vida."
         />
         <Testimonial
           img="Sarah-45de0e53b7b2520ebbac35c454b9748a"
@@ -28,6 +29,7 @@ function App() {
             job: "Ingeniera de Software",
           }}
           comment="freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble."
+          highlight="freeCodeCamp fue la puerta de entrada a mi carrera"
         />
         <Testimonial
           img="Sarah-45de0e53b7b2520ebbac35c454b9748a"
@@ -38,6 +40,7 @@ function App() {
             job: "Ingeniera de Software",
           }}
           comment="Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."
+          highlight="freeCodeCamp me dio las habilidades"
         />
       </section>
     </>
--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -6,11 +6,15 @@ function Testimonial(props) {
 
   // const imageSrc = require(`../assets/images/${props.img}.png`).default;
   // console.log(`../assets/images/${props.img}.png`);
+  const textParts = props.comment.split(
+    new RegExp(`(${props.highlight})`, "gi")
+  );
   return (
     <>
       <div className="testimonial">
         <img
           // src={require(`../assets/images/${props.img}`)}
+          // src={import.meta.glob(`../assets/images/${props.img}`)}
           // src="https://www.freecodecamp.org/static/Shawn-bf42c7b909dae3111d18a21870ee6ca5.png"
           alt={`Foto de ${props.name}`}
         />
@@ -21,7 +25,21 @@ function Testimonial(props) {
           <p className="title">
             {props.company.job} en <strong>{props.company.name}</strong>
           </p>
-          <p className="comment">"{props.comment}"</p>
+          <p className="comment">
+            {/* "{props.comment}" */}
+            {/* Highlighting the text */}
+            {/* {props.comment.replace(
+              `${props.highlight}`,
+              <strong>${props.highlight}</strong>
+            )} */}
+            {textParts.map((part, index) =>
+              part.toLowerCase() === props.highlight.toLowerCase() ? (
+                <strong key={index}>{part}</strong>
+              ) : (
+                part
+              )
+            )}
+          </p>
         </div>
       </div>
     </>
